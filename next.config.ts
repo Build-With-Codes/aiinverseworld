c83d10b5bcffcb6c3b6307f160217940931a53d7
manoharvirati <40772700+manoharvirati@users.noreply.github.com>
2026-07-22 12:59:39 +0530

    no message

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), payment=(), usb=(), accelerometer=(), gyroscope=(), magnetometer=()",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
